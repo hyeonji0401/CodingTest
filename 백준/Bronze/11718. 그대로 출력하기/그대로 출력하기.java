b4af2c121611be2ebfe22aefaf6bda57cf3d8e8e
hyeonji <106599092+hyeonji0401@users.noreply.github.com>
2024-02-20 14:57:54 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 132 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -1,14 +1,18 @@
-import java.util.*;
+import java.io.*;
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		while(sc.hasNextLine()) {
-			String str = sc.nextLine();
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str;
+		
+		while((str=br.readLine())!=null) {
 			System.out.println(str);
 		}
-		sc.close();
+		
+		br.close();
+		
 	}
 
 }
