1b7c1a2f671c0bf9fa41114ab08c11c7efc9a2ef
hyeonji <106599092+hyeonji0401@users.noreply.github.com>
2024-03-05 17:20:30 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 132 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -1,22 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 	
-		Scanner sc = new Scanner(System.in);
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		
 		String str[]= {"c=","c-","dz=", "d-","lj","nj","s=","z="};
 		
-		String st = sc.next();
+		String st = bf.readLine();
 		
 		for(int i=0; i<str.length; i++)
 		{
+			//contains: 대상 문자열에 특정 문자열이 포함되어있는지 확인
 			if(st.contains(str[i]))
 				st = st.replace(str[i], "!");
+			//찾으려는 문자에 포함안되는 문자로 바꿔줘야함
 		}
 		
 		System.out.println(st.length());
-		
 	}
-
 }
