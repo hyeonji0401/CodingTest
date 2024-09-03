52194868dd0cbadfbcd1c02b7d01aaf6aa47d0e7
hyeonji <106599092+hyeonji0401@users.noreply.github.com>
2024-09-03 20:41:42 +0900

    [Bronze II] Title: 분해합, Time: 184 ms, Memory: 17660 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -4,30 +4,22 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int num = sc.nextInt();
-		int count=0;
 		int sum=0;
+		int result=0;
 		
-		for(int j=0; j<7; j++) {
-			if((num/Math.pow(10, j))>=1) {
-				count++;
+		for(int i=1; i<num; i++) {
+			sum=0;
+			int cal = i;
+			while(cal!=0) {
+				sum+=cal%10;
+				cal/=10;
 			}
-			else {
+			if(sum+i==num) {
+				result=i;
 				break;
 			}
 		}
-		
-		for(int i=1; i<=num; i++) {
-			for(int j=0; j<count;j++) { 
-				sum+=((i/Math.pow(10, j))%10);
-				
-			}
-			if(sum+i==num) {
-			  System.out.print(i);
-			  System.exit(0);
-			}
-			sum=0;
-		}
-		System.out.print(0);
+		System.out.println(result);
 
 	}
 
