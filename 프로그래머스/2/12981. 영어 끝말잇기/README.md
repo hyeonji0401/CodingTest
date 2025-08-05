c40ef32b11a4a6d19a7734ce54d720c7d7a63f24
hyeonji <106599092+hyeonji0401@users.noreply.github.com>
2025-08-05 11:53:44 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/README.md
+++ 프로그래머스/2/12981. 영어 끝말잇기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 영어 끝말잇기 - 12981 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981?language=cpp) 
 
 ### 성능 요약
 
-메모리: 4.14 MB, 시간: 0.05 ms
+메모리: 4.21 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 05일 11:48:51
+2025년 08월 05일 11:53:42
 
 ### 문제 설명
 
--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.cpp
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.cpp
@@ -11,31 +11,16 @@ vector<int> solution(int n, vector<string> words) {
     for(int i=0; i<words.size(); i++)
     {
         if(i==0) continue;
-        if(words[i-1].back() != words[i].front())
+        auto it = find(words.begin(), words.begin()+i, words[i]);
+        if(words[i-1].back() != words[i].front()||it!=words.begin()+i)
         {
-            answer.insert(answer.begin(),i%n+1);
-            answer.insert(answer.begin()+1,i/n+1);
+            answer.push_back(i%n+1);
+            answer.push_back(i/n+1);
             return answer;
         }
-        else
-        {
-            auto it = find(words.begin(), words.begin()+i, words[i]);
-            if(it!=words.begin()+i)
-            {
-                std::cout <<  i << std::endl;
-                answer.insert(answer.begin(),i%n+1);
-                answer.insert(answer.begin()+1,i/n+1);
-                return answer;
-            }
-        }
-        
-        if(i==words.size()-1)
-        {
-            answer.insert(answer.begin(),0);
-            answer.insert(answer.begin()+1,0);
-            return answer;
-        }
+       
     }
+    answer={0,0};
 
     return answer;
 }
