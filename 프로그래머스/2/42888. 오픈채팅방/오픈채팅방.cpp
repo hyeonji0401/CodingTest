625f23cc2a1826269652236db8e66bef109a29f7
hyeonji <106599092+hyeonji0401@users.noreply.github.com>
2025-08-06 11:07:05 +0900

    [level 2] Title: 오픈채팅방, Time: 225.29 ms, Memory: 38.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.cpp
@@ -0,0 +1,46 @@
+#include <string>
+#include <sstream>
+#include <vector>
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+vector<string> solution(vector<string> record) {
+    vector<string> answer;
+    unordered_map<string, string>user;
+    
+    for(int i=0; i<record.size(); i++)
+    {
+        stringstream ss(record[i]);
+        string action, id, name;
+        ss >> action >> id >> name;
+        if(action.compare("Leave")==0) continue;
+        auto isExist = user.insert({id, name});
+        if(!isExist.second&& (!user[id].compare(name)==0))
+        {
+            user.erase(id);
+            user.insert({id,name});
+        }
+    }
+    
+    for(int j=0; j<record.size(); j++)
+    {
+        stringstream ss(record[j]);
+        string action, id, name;
+        ss >> action >> id >> name;
+
+        if(action.compare("Enter")==0)
+        {
+            string enter = user[id]+"님이 들어왔습니다.";
+            answer.push_back(enter);
+        }
+        else if(action.compare("Leave")==0)
+        {
+            string leave = user[id]+"님이 나갔습니다.";
+            answer.push_back(leave);
+        }
+    }
+    
+    return answer;
+}
